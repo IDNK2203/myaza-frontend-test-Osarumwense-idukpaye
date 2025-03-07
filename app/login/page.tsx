21e0a49c053a26312499e7625bf0e661db3ec4eb
Idnk <efeidukpaye@gmail.com>
2025-03-08 00:14:31 +0100

    add logo1 component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,14 @@
 import Image from "next/image";
 import Logo1 from "../../public/images/logo-1.png";
-
 import { LoginForm } from "@/components/login-form";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-col flex-1 items-center justify-center">
-          <div className="w-full flex justify-center max-w-28">
-            <Image alt="Brand Logo" src={Logo1} className="w-full" />{" "}
-          </div>
-
+          <Logo />
           <div className="w-full max-w-xs">
             <LoginForm />
           </div>
@@ -27,3 +24,13 @@ export default function LoginPage() {
     </div>
   );
 }
+
+const Logo = () => {
+  return (
+    <div className="w-full flex justify-center max-w-28">
+      <Link href="#" className="flex items-center gap-2 font-medium">
+        <Image alt="Brand Logo" src={Logo1} className="w-full" />{" "}
+      </Link>
+    </div>
+  );
+};
