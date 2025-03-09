52b2fec58cfb61c831716f14db8b90f017fe072f
Idnk <efeidukpaye@gmail.com>
2025-03-09 12:06:09 +0100

    fix theme hydration bug

--- components/ui/mode-toggle.tsx
+++ components/ui/mode-toggle.tsx
@@ -1,5 +1,4 @@
-// "use client";
-
+"use client";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
@@ -10,9 +9,18 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useEffect, useState } from "react";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
+  const [themeState, setThemeState] = useState<string | undefined>("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Client-side only code
+      setThemeState(theme);
+    }
+  }, [theme]);
 
   return (
     <DropdownMenu>
@@ -23,7 +31,7 @@ export function ModeToggle() {
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="capitalize">{theme} Theme</span>
+          <span className="capitalize">{themeState} Theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
